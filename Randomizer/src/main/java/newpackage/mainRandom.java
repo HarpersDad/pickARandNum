6f9aa57291532d98391321a23caede5c18a826f0
Clifton Lindsey <42348662+HarpersDad@users.noreply.github.com>
2023-04-25 09:27:06 -0400

    Update mainRandom.java
    updates

--- Randomizer/src/main/java/newpackage/mainRandom.java
+++ Randomizer/src/main/java/newpackage/mainRandom.java
@@ -1,12 +1,11 @@
-
-package newpackage;
-
 import java.util.*;
 
 public class mainRandom 
 {
     public static Random rand = new Random();
     public static Scanner in = new Scanner (System.in);
+    public static int thisNum = 101;
+    public static int finalNum = 0;
 
     public static List<String> peopleList = new ArrayList<String>();
     public static List<Integer> numberList = new ArrayList<Integer>(); 
@@ -15,10 +14,9 @@ public class mainRandom
     public static int userMenu()
     {
         System.out.println("Please choose an option: "
-                + "\n1) add person to list"
-                + "\n2) run Randomizer"
-                + "\n3) run final output"
-                + "\n4) Exit"
+                + "\n1) add person to list and assign random number"
+                + "\n2) run final output"
+                + "\n3) Exit"
                 + "\n");
         int option = in.nextInt();
         
@@ -31,17 +29,10 @@ public class mainRandom
         {
             case 1:
                 addUser();
-                System.out.println("####################################################\n");
                 userOption(userMenu());
                 break;
                 
             case 2:
-                addRandom();
-                System.out.println("####################################################\n");
-                userOption(userMenu());
-                break;
-                
-            case 3:
                 finalRandom();
                 break;
                 
@@ -55,6 +46,7 @@ public class mainRandom
         System.out.println("Enter first name: \n");
         String name = in.next();
         peopleList.add(name);
+        addRandom();
     }
     
     public static void addRandom()
@@ -82,8 +74,27 @@ public class mainRandom
             System.out.println(peopleList.get(i) + "'s number is " + numberList.get(i));
         }
         
-        System.out.println("The final random number is: " + rand.nextInt(101));
+        finalNum = rand.nextInt(101);
+        System.out.println("The final random number is: " + finalNum);
+        System.out.println("The person with the closest number is " + peopleList.get(closestChoice()));
     }
+    
+    public static int closestChoice()
+    {
+    	int num = 0;
+    	
+    	for	(int i = 0; i < peopleList.size(); i++)
+    	{   
+    		if (thisNum > Math.abs(finalNum - numberList.get(i)))
+    		{
+    			thisNum = Math.abs(finalNum - numberList.get(i));
+    			num = i;
+    		}
+    	}
+    	
+    	return num;
+    }
+    
     public static void main(String args[])
     {
         int userChoice = userMenu();
